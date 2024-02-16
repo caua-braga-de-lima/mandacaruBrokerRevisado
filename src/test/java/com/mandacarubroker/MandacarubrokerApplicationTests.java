d05a2a948f16fe6180fff386d052f46b8b0c9856
Cauã Braga de Lima <cauabrgal11@gmail.com>
2024-02-16 15:32:15 -0300

    Initial commit

---
+++ src/test/java/com/mandacarubroker/MandacarubrokerApplicationTests.java
@@ -0,0 +1,21 @@
+package com.mandacarubroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandacarubrokerApplicationTests {
+
+	@Test
+	void contextLoads()
+	{
+
+	}
+
+}
+
+
+
+
+
+
